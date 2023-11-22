435e0f48d624331309c18904775ecab3c616db0d
Komalidangeti <147477535+Komalidangeti@users.noreply.github.com>
2023-11-22 14:39:15 +0530

    #include<stdio.h> int main() {   float bs, hra, da;   scanf("%f%f%f",&bs,&hra,&da);   float pf=(12.0/100.0)*bs;   float gs=bs+hra+da+pf;   printf("%.2f ",pf);   printf("%.2f ",gs); }

---
+++ Calculate_Gross_Salary_of_an_employee.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() 
+{
+  float bs, hra, da;
+  scanf("%f%f%f",&bs,&hra,&da);
+  float pf=(12.0/100.0)*bs;
+  float gs=bs+hra+da+pf;
+  printf("%.2f
+",pf);
+  printf("%.2f
+",gs);
+}
